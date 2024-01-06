6ca24bfb4754a1303f4b429477f88f93eecb8fca
ame-gubay <amaregubay11@gmail.com>
2024-01-06 21:06:05 +0300

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text> well come Home page!</Text>
     </View>
   );
 }
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text> well come to Settings page!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" options={{
+            tabBarIcon: ({ focused, color, size }) => {
+              return (
+                <Ionicons
+                  name={focused ? "home" : "home-outline"}
+                  size={size}
+                  color={color}
+                />
+              );
+            },
+          }}component={HomeScreen} />
+      <Tab.Screen name="Settings" 
+      options={{
+        tabBarIcon: ({ focused, color, size }) => {
+          return (
+            <Ionicons
+              name={focused ? "settings" : "settings-sharp"}
+              size={size}
+              color={color}
+            />
+          );
+        },
+      }}
+      component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
